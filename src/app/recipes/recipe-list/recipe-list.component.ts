7c382e2a505ec3314ad19d73e5fc8695073733a4
Nitin <nitin3emails@gmail.com>
2021-11-13 18:32:40 +0530

    Compled Section 6

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] = [
     new Recipe("Butter Chicken",`Simple Butter chicken recipe`,`https://www.indianhealthyrecipes.com/wp-content/uploads/2014/09/butter-chicken-500x500.jpg`)
-    ,new Recipe("Butter Chicken",`Simple Butter chicken recipe`,`https://www.indianhealthyrecipes.com/wp-content/uploads/2014/09/butter-chicken-500x500.jpg`)
+    ,new Recipe("Shahi Paneer",`Simple Shahi Paneer`,`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_LaRfeM-EkUbJ8MXtHafkxpluBjDXpTSHYg&usqp=CAU`)
   ]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild,EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput',{static:true}) nameInput:ElementRef;
+  @ViewChild('amountInput',{static:true}) amountInput:ElementRef;
+  @Output() ingredientAdded = new EventEmitter<{name:string,amount:number}>();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onAddItem(){
+    const inName = this.nameInput.nativeElement.value;
+    const inAmount = this.amountInput.nativeElement.value;
+    const newIngredient = new Ingredient(inName,inAmount)
+    this.ingredientAdded.emit(newIngredient);
+  }
 }

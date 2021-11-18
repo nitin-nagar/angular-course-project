04ca3427567aee2764b39276d9f55dac57cd7490
Nitin Kumar <nitin1@unthinkable.co>
2021-11-18 12:22:09 +0530

    Changed EventEmitter to Subject and next instead of emit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   // ingredients:Ingredient[] = [
   //   new Ingredient('Apple',5),
   //   new Ingredient('Orange',10),
   // ];
-  ingredients:Ingredient[];
-  constructor(private shoppingListService:ShoppingListService) { }
+  ingredients: Ingredient[];
+  private igChangeSubs: Subscription;
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChnage.subscribe(
-      (ingredients:Ingredient[])=>{
+    this.igChangeSubs = this.shoppingListService.ingredientsChnage.subscribe(
+      (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
-    )
+    );
   }
-  onIngredientAdded(ingredient:Ingredient){
+  ngOnDestroy() {
+    this.igChangeSubs.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,24 +1,25 @@
-import { EventEmitter } from "@angular/core";
-import { Ingredient } from "../shared/ingredient.model";
+import { Subject, Subscription } from 'rxjs';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-    ingredientsChnage = new EventEmitter<Ingredient[]>();
-    private ingredients:Ingredient[] = [
-        new Ingredient('Apple',5),
-        new Ingredient('Orange',10),
-      ];
-    getIngredients(){
-        return this.ingredients.slice();
-    }  
-    addIngredient(ingredient:Ingredient){
-        this.ingredients.push(ingredient);
-        this.ingredientsChnage.emit(this.ingredients.slice());
-    }
-    addIngredients(ingredients:Ingredient[]) {
-        // for(let ingredient of ingredients){
-        //     this.addIngredient(ingredient);
-        // }
-        this.ingredients.push(...ingredients);
-        this.ingredientsChnage.emit(this.ingredients.slice())
-    }
+  ingredientsChnage = new Subject<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Orange', 10),
+  ];
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChnage.next(this.ingredients.slice());
+  }
+  addIngredients(ingredients: Ingredient[]) {
+    // for(let ingredient of ingredients){
+    //     this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
+    this.ingredientsChnage.next(this.ingredients.slice());
+  }
 }
